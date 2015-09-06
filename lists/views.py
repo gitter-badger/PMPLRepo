9e0f026a6a5e8017f8447c7bc9558c916d598d90
tyashandayani <tyashandayani95@gmail.com>
2015-09-06 15:45:34 +0000

    commit lagi homepage dan clone

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	return HttpResponse('<html><title>To-Do lists</title><body>Nama : Tyas Kusuma H<br>NPM : 1206208214</body></html>')
